56a651e5ebb82034e64dd144281cc3b1a5c89efd
TailC <wlxlj24@qq.com>
2016-06-01 09:39:12 +0800

    init project

---
+++ iOSCoreAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSCoreAnimationDemo
+//
+//  Created by TailC on 16/6/1.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSCoreAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSCoreAnimationDemo
+//
+//  Created by TailC on 16/6/1.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
