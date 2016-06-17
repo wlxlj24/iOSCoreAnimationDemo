5714aac581fd61e56e08675c6cc325680339a08d
TailC <wlxlj24@qq.com>
2016-06-17 11:38:34 +0800

    使用CATextLayer的UILabel子类：LayerLabel

---
+++ iOSCoreAnimationDemo/ViewController29.h
@@ -0,0 +1,13 @@
+//
+//  ViewController29.h
+//  iOSCoreAnimationDemo
+//
+//  Created by TailC on 16/6/17.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController29 : UIViewController
+
+@end
---
+++ iOSCoreAnimationDemo/ViewController29.m
@@ -0,0 +1,25 @@
+//
+//  ViewController29.m
+//  iOSCoreAnimationDemo
+//
+//  Created by TailC on 16/6/17.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import "ViewController29.h"
+
+@interface ViewController29 ()
+
+@end
+
+@implementation ViewController29
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+	
+	
+}
+
+
+@end
