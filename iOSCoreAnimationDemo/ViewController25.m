674bf5b912a3e205f0617ed7f81c4265a5c4c9cd
TailC <wlxlj24@qq.com>
2016-06-17 10:18:23 +0800

    绘制火柴人

--- iOSCoreAnimationDemo/ViewController25.m
+++ iOSCoreAnimationDemo/ViewController25.m
@@ -10,28 +10,51 @@
 
 @interface ViewController25 ()
 
+@property (nonatomic,readwrite,strong) UIView *containView;
+
 @end
 
 @implementation ViewController25
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
+	
+	self.containView = ({
+		UIView *var = [UIView new];
+		[self.view addSubview:var];
+		[var mas_makeConstraints:^(MASConstraintMaker *make) {
+			make.left.mas_equalTo(var.superview.mas_left);
+			make.right.mas_equalTo(var.superview.mas_right);
+			make.top.mas_equalTo(var.superview.mas_top).mas_offset(100);
+			make.bottom.mas_equalTo(var.superview.mas_bottom).mas_offset(-100);
+		}];
+		var.backgroundColor = [UIColor groupTableViewBackgroundColor];
+		
+		var;
+	});
+	
+	UIBezierPath *path = [[UIBezierPath alloc] init];
+	[path moveToPoint:CGPointMake(175, 100)];
+	
+	[path addArcWithCenter:CGPointMake(150, 100) radius:25 startAngle:0 endAngle:2*M_PI clockwise:YES];
+	[path moveToPoint:CGPointMake(150, 125)];
+	[path addLineToPoint:CGPointMake(150, 175)];
+	[path addLineToPoint:CGPointMake(125, 225)];
+	[path moveToPoint:CGPointMake(150, 175)];
+	[path addLineToPoint:CGPointMake(175, 225)];
+	[path moveToPoint:CGPointMake(100, 150)];
+	[path addLineToPoint:CGPointMake(200, 150)];
+	
+	CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+	shapeLayer.strokeColor = [UIColor redColor].CGColor;
+	shapeLayer.fillColor = [UIColor clearColor].CGColor;
+	shapeLayer.lineWidth = 5.0f;
+	shapeLayer.lineJoin = kCALineJoinRound;
+	shapeLayer.lineCap = kCALineCapRound;
+	shapeLayer.path = path.CGPath;
+	[self.containView.layer addSublayer:shapeLayer];
+	
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
